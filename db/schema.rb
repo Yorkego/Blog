539ed67f6393037e3dbb4dfc940e2c30c74e48fa
Yorkego <Yorkego1@ukr.net>
2018-04-02 14:02:30 +0300

    Create contact migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401104536) do
+ActiveRecord::Schema.define(version: 20180402105621) do
 
   create_table "articals", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
